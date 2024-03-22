2e4cda4973db37648eef5cf29e1d83ed93f4d3ef
sreekandukuri <kandukuriprasadu@gmail.com>
2024-03-22 12:57:09 +0530

    Test

---
+++ Sources/PrasadFW.xcframework/ios-arm64/PrasadFW.framework/Headers/PrasadFW.h
@@ -0,0 +1,18 @@
+//
+//  PrasadFW.h
+//  PrasadFW
+//
+//  Created by Prasad Kandukuri on 22/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrasadFW.
+FOUNDATION_EXPORT double PrasadFWVersionNumber;
+
+//! Project version string for PrasadFW.
+FOUNDATION_EXPORT const unsigned char PrasadFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrasadFW/PublicHeader.h>
+
+
